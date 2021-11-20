7a9a08e332270ae384f243236083f34bdb3c3d65
Ray <ray@example.com>
2021-11-20 21:48:31 +0100

    Subfiltering works

--- Crashboard_draft/dashboard.js
+++ Crashboard_draft/dashboard.js
@@ -55,11 +55,15 @@ function create_table(json, table_div_id) {
 }
 
 MAIN_FORM_STATE = {
-  'city': 'WIEN'
+  'country': 'AT',
+  'city': 'UNSET'
 }
 
 function filter_data(json) {
-  filtered = json.filter(d => d['VendorCity'] == MAIN_FORM_STATE['city'])
+  filtered = json.filter(d => d['VendorCountry'] == MAIN_FORM_STATE['country'])
+  if (MAIN_FORM_STATE['city'] != "UNSET") {
+    filtered = json.filter(d => d['VendorCity'] == MAIN_FORM_STATE['city'])
+  }
   return filtered
 }
 
@@ -68,8 +72,8 @@ function create_select_element(text_list, state_changer_function) {
   select.classList = `form-select form-select-sm`
   text_list.forEach(d => {
     const option = document.createElement('option')
-    option.textContent = d
-    option.value = d
+    option.textContent = (d.full_name !== null) ? d.full_name : d.code_name
+    option.value = d.code_name
     select.appendChild(option)
   })
   select.addEventListener('change', (e) => {
@@ -79,25 +83,42 @@ function create_select_element(text_list, state_changer_function) {
   return select
 }
 
-
-function load_search_form(json, city_data, country_data) {
-  input_id = 'manufacture-search-form'
-  const seach_form = document.getElementById(input_id)
-
-
+function load_city_form(json, city_data, geo_data, chosen_code_name) {
   // City
-  const list_of_cities = city_data.map(d => d.name)
+  const filtered_country_cities = geo_data.filter(d => d.code_name == chosen_code_name)[0].cities
+  const list_of_cities = filtered_country_cities.map(d => { return {'full_name': d.full_name, 'code_name': d.code_name}})
   const city_select = create_select_element(list_of_cities, (selected_value) => {
     MAIN_FORM_STATE.city = selected_value
-    load_graphs(json, city_data, country_data)
+    render(json, city_data, geo_data)
   })
 
   const city_col_4 = document.createElement('div')
   city_col_4.className = 'col-4'
   city_col_4.appendChild(city_select)
+  return city_col_4
+}
 
 
-  seach_form.appendChild(city_col_4)
+function load_search_form(json, city_data, geo_data) {
+  input_id = 'manufacture-search-form'
+  const seach_form = document.getElementById(input_id)
+  const sub_form = document.createElement('div')
+
+  // country
+  const list_of_countries = geo_data.map(d => { return {'full_name': d.full_name, 'code_name': d.code_name}})
+  const country_select = create_select_element(list_of_countries, (selected_value) => {
+    MAIN_FORM_STATE.country = selected_value
+    render(json, city_data, geo_data)
+    sub_form.innerHTML = ``
+    sub_form.appendChild(load_city_form(json, city_data, geo_data, selected_value))
+  })
+
+  const country_col_4 = document.createElement('div')
+  country_col_4.className = 'col-4'
+  country_col_4.appendChild(country_select)
+
+  seach_form.appendChild(country_col_4)
+  seach_form.appendChild(sub_form)
 }
 
 
@@ -106,12 +127,15 @@ function update_results_counter(length) {
   counter.textContent = length
 }
 
-function load_graphs(json, city_data, country_data) {
+function render(json, city_data, geo_data) {
+  json = filter_data(json)
+  load_graphs(json, city_data, geo_data)
+}
+
+function load_graphs(json, city_data, geo_data) {
   const table_results = document.getElementById('manufacturer-list-of-results')
   table_results.className = `table-scroll-vertical`
   table_results.innerHTML = `` // TODO, also do the same for diagrams!
-
-  json = filter_data(json)
 
   update_results_counter(json.length)
   // json = json.slice(0, 4) // TEMPORARY!!!
@@ -147,17 +171,17 @@ function load_graphs(json, city_data, country_data) {
 
 
 // Get the file
-fetch("../json_data/spend_data/spend_data_not_empty.json")
+fetch("../json_data/spend_data/spend_data.json")
   .then(response => response.json())
   .then(json => {
-    fetch("../json_data/country_data/country_data.json")
+    fetch("../json_data/city_data/city_data.json")
       .then(response => response.json())
-      .then(country_data => {
-          fetch("../json_data/city_data/city_data.json")
+      .then(city_data => {
+          fetch("../json_data/geo_data/geo_data.json")
           .then(response => response.json())
-          .then(city_data => {
-              load_search_form(json, city_data, country_data)
-              load_graphs(json, city_data, country_data)
+          .then(geo_data => {
+              load_search_form(json, city_data, geo_data)
+              render(json, city_data, geo_data)
           });
       });
   });
