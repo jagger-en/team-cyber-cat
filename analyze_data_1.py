9f053f571baba3845c9074e99bca6b2a0d058070
Ray <ray@example.com>
2021-11-20 11:07:21 +0100

    Verified calc_statistics

--- analyze_data_1.py
+++ analyze_data_1.py
@@ -6,21 +6,13 @@ DATA_FOLDER = 'json_data'  # DO NOT CHANGE!
 df = pd.read_excel('SIEVO JUNCTION Spend data.xlsx')
 df = df[df['ProductId'].isna()]
 df_germany = df[df['VendorCountry'] == 'DE']
-print(df_germany)
-
-def calc_statistics():
-    pass
-    return {
-        'average': '',
-        'max': '',
-        'min': '',
-    }
+print(df_germany.columns)
 
 
-utils.clean_directory(DATA_FOLDER)
-utils.output_to_file(f'{DATA_FOLDER}/emissions_per_eur', 'above_5', result)
-utils.output_to_file(f'{DATA_FOLDER}/emissions_per_usd', 'above_5', result)
-utils.output_to_file(f'{DATA_FOLDER}/emissions_per_usd', 'below_5', result)
+# TODO: Later export
+# utils.clean_directory(DATA_FOLDER)
+# utils.output_to_file(f'{DATA_FOLDER}/emissions_per_eur', 'above_5', result)
+
 
 
 def calc_statistics(df, numeric_column_name):
@@ -30,3 +22,7 @@ def calc_statistics(df, numeric_column_name):
     mean = numeric_column.mean()
     res = {'max': max, 'min': min, 'mean': mean}
     return res
+
+
+stats_germany = calc_statistics(df_germany, 'SpendOriginalCurrency')
+print(stats_germany)
