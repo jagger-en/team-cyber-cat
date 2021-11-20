e767cf1a6954a8bbc9a05b3c941cb720c21bcf95
votth <voth998@gmai.com>
2021-11-20 20:29:05 +0100

    Merged with Shuang's selection

--- Crashboard_draft/main.js
+++ Crashboard_draft/main.js
@@ -1,134 +1,100 @@
+// The map
+var map = L.map("map").setView([48.3794, 31.1656], 3);
+// Single layer/marker
+var ATmarker;
+var marker;
+// Layers/Markers groups
+var cities = new L.layerGroup();
+var warnings = new L.layerGroup();
 
+L.tileLayer(
+  "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}@2x.png?key=RqPVefXUdjgIrKWZw5Nh",
+  {
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  }
+).addTo(map);
 /*window.onload = function selectedCountry(){
     var input = document.getElementById('country');
     console.log(input);*/
-input = "China"
+input = "Germany";
 
+function load_map(json) {
+  const coordinates = json;
+  var len = coordinates.length;
 
-if(input=="ALL"){
+  for (var i = 0; i < len; i++) {
+    ATmarker = L.marker([coordinates[i].coords.lat, coordinates[i].coords.long])
+      .bindPopup(
+        coordinates[i].full_name + "(" + coordinates[i].code_name + ")"
+      )
+      .openPopup();
+    cities.addLayer(ATmarker);
+    /*map.setView([coordinates[i].coords.lat,coordinates[i].coords.long],8);*/
+  }
+  cities.addTo(map);
 
-    function load_map(json) {
-        var map = L.map("map").setView([48.3794, 31.1656], 3);
-        L.tileLayer(
-        "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}@2x.png?key=RqPVefXUdjgIrKWZw5Nh",
-        {
-            attribution:
-            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-        }
-        ).addTo(map);
-    
-      
-    
-        const coordinates= json;
-    
-        var len= coordinates.length;
-    
-        for(var i = 0; i<len;i++){
-            var ATmarker = L.marker([coordinates[i].coords.lat, coordinates[i].coords.long])
-            .addTo(map)
-            .bindPopup(coordinates[i].full_name+"("+coordinates[i].code_name+")")
-            .openPopup();
-            /*map.setView([coordinates[i].coords.lat,coordinates[i].coords.long],8);*/
-        }
-    
-    
-        var popup = L.popup();
-        /*function selectedCountry(e){
+  var popup = L.popup();
+  /*function selectedCountry(e){
             map.setView([e.latlng.lat,e.latlng.lng],12);
         }*/
-        function onMapClick(e) {
-            popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
-        }
-        map.on('click', onMapClick);
-    }
-    
-    
-    
-    // Get the file
-    fetch("../json_data/geo_data/geo_data.json")
-      .then(response => response.json())
-      .then(json => {
-          console.log(json)
-          load_map(json)
-      });
-
+  function onMapClick(e) {
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(map);
+  }
+  map.on("click", onMapClick);
 }
 
-else{
-    function load_map(json) {
-        var map = L.map("map").setView([48.3794, 31.1656], 3);
-        L.tileLayer(
-        "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}@2x.png?key=RqPVefXUdjgIrKWZw5Nh",
-        {
-            attribution:
-            '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-        }
-        ).addTo(map);
-    
-      
-    
-        const coordinates= json;
-    
-        var len= coordinates.length;
-    
-        for(var i = 0; i<len;i++){
-            if(coordinates[i].full_name==input){
-                var coorIndex=i;
-            }
-        }
-
-       
-        var marker = L.marker([coordinates[coorIndex].coords.lat, coordinates[coorIndex].coords.long])
-        .addTo(map)
-        .bindPopup(coordinates[coorIndex].full_name+"("+coordinates[coorIndex].code_name+")")
-        .openPopup();
-        map.setView([coordinates[coorIndex].coords.lat,coordinates[coorIndex].coords.long],8);
-        
-    
-    
-        var popup = L.popup();
-        /*function selectedCountry(e){
-            map.setView([e.latlng.lat,e.latlng.lng],12);
-        }*/
-        function onMapClick(e) {
-            popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
-        }
-        map.on('click', onMapClick);
-    }
-    
-    
-    
-    // Get the file
-    fetch("../json_data/geo_data/geo_data.json")
-      .then(response => response.json())
-      .then(json => {
-          console.log(json)
-          load_map(json)
-      });
+function warningClick(cb) {
+  if (cb.checked && map.hasLayer(cities)) {
+    map.removeLayer(cities);
+    map.removeLayer(marker);
+    // map.addLayer(warnings);
+    map.setView([48.3794, 31.1656], 3);
+  } else {
+    // map.removeLayer(warnings);
+    map.addLayer(cities);
+  }
 }
-//}
 
+if (input == "ALL") {
+  map.addLayer(cities);
+} else {
+  function load_map(json) {
+    const coordinates = json;
+    var len = coordinates.length;
 
+    for (var i = 0; i < len; i++) {
+      if (coordinates[i].full_name == input) {
+        var coorIndex = i;
+      }
+    }
 
+    marker = L.marker([
+      coordinates[coorIndex].coords.lat,
+      coordinates[coorIndex].coords.long,
+    ])
+      .bindPopup(
+        coordinates[coorIndex].full_name +
+          "(" +
+          coordinates[coorIndex].code_name +
+          ")"
+      )
+      .openPopup();
+    map.addLayer(marker);
+    map.setView(
+      [coordinates[coorIndex].coords.lat, coordinates[coorIndex].coords.long],
+      8
+    );
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+// Get the file
+fetch("../json_data/geo_data/geo_data.json")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+    load_map(json);
+  });
