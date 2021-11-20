f273fb4e1d5bb28783abfb3ed589f6d6f980dd06
yw-scu <49891048+yw-scu@users.noreply.github.com>
2021-11-20 10:59:03 +0100

    Implemented function 'calc_statistics'.

--- analyze_data_1.py
+++ analyze_data_1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 from lib import utils
-DATA_FOLDER = 'json_data' # DO NOT CHANGE!
+DATA_FOLDER = 'json_data'  # DO NOT CHANGE!
 
 df = pd.read_excel('SIEVO JUNCTION Emissions per EUR.xlsx')
 
@@ -13,3 +13,12 @@ utils.clean_directory(DATA_FOLDER)
 utils.output_to_file(f'{DATA_FOLDER}/emissions_per_eur', 'above_5', result)
 utils.output_to_file(f'{DATA_FOLDER}/emissions_per_usd', 'above_5', result)
 utils.output_to_file(f'{DATA_FOLDER}/emissions_per_usd', 'below_5', result)
+
+
+def calc_statistics(df, numeric_column_name):
+    numeric_column = df[numeric_column_name]
+    max = numeric_column.max()
+    min = numeric_column.min()
+    mean = numeric_column.mean()
+    res = {'max': max, 'min': min, 'mean': mean}
+    return res
