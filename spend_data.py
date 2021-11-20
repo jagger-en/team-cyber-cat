1c663e4ea697f6d974cf72f8d91f31ae75b738ec
yw-scu <49891048+yw-scu@users.noreply.github.com>
2021-11-20 09:21:04 +0100

    test

---
+++ spend_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df_emissions_eur = pd.read_excel('SIEVO JUNCTION Emissions per EUR.xlsx')
+df_emissions_uom = pd.read_excel('SIEVO JUNCTION Emissions per UOM.xlsx')
+df_spend_data = pd.read_excel('SIEVO JUNCTION Spend data.xlsx')
+
